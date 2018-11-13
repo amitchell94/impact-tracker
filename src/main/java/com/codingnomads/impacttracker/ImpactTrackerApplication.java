eae695386f556dcbd17b8ef14c371b87aa50c854
Andy <andymitchell1@live.co.uk>
2018-11-13 16:08:40 -0500

    first commit

---
+++ src/main/java/com/codingnomads/impacttracker/ImpactTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.codingnomads.impacttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpactTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImpactTrackerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/codingnomads/impacttracker/ImpactTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codingnomads.impacttracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImpactTrackerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
