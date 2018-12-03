147ff2c7bec80ab48a3170ff577dddd0151fc79b
Andy Mitchell <42553271+amitchell94@users.noreply.github.com>
2018-12-03 17:25:54 -0500

    * Added war dependency (#23)
    * Made application extend SpringBootServletInitializer
    * Added host environment variables

--- src/main/java/com/codingnomads/impacttracker/ImpactTrackerApplication.java
+++ src/main/java/com/codingnomads/impacttracker/ImpactTrackerApplication.java
@@ -2,10 +2,11 @@ package com.codingnomads.impacttracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class ImpactTrackerApplication {
+public class ImpactTrackerApplication extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) {
