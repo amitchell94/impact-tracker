dc6e7d71d6ad98b8d2b42b7c1b9d7f16d95957c6
Andrea Howes <26912903+ashcreek@users.noreply.github.com>
2018-12-04 14:18:52 -0500

    Andrea commitment page tests (#24)
    * in progress for testing commitment controller and service
    
    * added all commitment service tests
    
    * fixed naming on updateCommitment Test

--- src/test/java/com/codingnomads/impacttracker/logic/CommitmentServiceTest.java
+++ src/test/java/com/codingnomads/impacttracker/logic/CommitmentServiceTest.java
@@ -11,8 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class CommitmentServiceTest {
 
@@ -21,6 +20,8 @@ public class CommitmentServiceTest {
     private CommitmentService commitmentService;
     private Commitment commitment1;
     private Commitment commitment2;
+    private Commitment inputCommitment;
+    private Commitment returnedCommitment;
     private Commitment commitmentFromRepository;
 
     @Before
@@ -31,7 +32,9 @@ public class CommitmentServiceTest {
         commitment1.setId(1);
         commitment2 = new Commitment();
         commitment2.setId(2);
-
+        inputCommitment = new Commitment();
+        inputCommitment.setReductionId(1);
+        returnedCommitment = new Commitment();
         commitmentFromRepository = new Commitment();
     }
 
@@ -54,6 +57,32 @@ public class CommitmentServiceTest {
         verify(mySqlCommitmentRepositoryMock).getCommitmentsFromUserId(VALID_USER_ID);
         assertThat(commitmentList).hasSize(2);
         assertThat(commitmentList).contains(commitment1,commitment2);
+    }
+
+    @Test
+    public void whenUpdatingCommitmentById_shouldReturnUpdatedCommitment(){
+        when(mySqlCommitmentRepositoryMock.updateCommitmentById(1, inputCommitment)).thenReturn(returnedCommitment);
+
+        Commitment updatedCommitment = commitmentService.updateCommitmentById(1, inputCommitment);
+
+        assertThat(updatedCommitment).isEqualTo(returnedCommitment);
+    }
+
+    @Test
+    public void whenDeletingCommitmentById_shouldDeleteCommitmentWithId(){
+      commitmentService.deleteCommitmentById(2);
+
+      verify(mySqlCommitmentRepositoryMock, times(1)).deleteCommitmentById(2);
+    }
+
+    @Test
+    public void whenGettingCommitmentById_shouldGetCommitmentWithId(){
+        when(mySqlCommitmentRepositoryMock.getCommitmentById(1)).thenReturn(commitment1);
+
+        Commitment commitmentById = commitmentService.getCommitmentById(1);
+
+        assertThat(commitmentById)
+                .extracting("id").containsOnly(1);
     }
 
 }
