fe517a1c9d84fbc64e32bcf30f7d2f8bfa67af44
mikram5 <mike.ramey@gmail.com>
2018-12-11 10:55:29 -0600

    Mike userservice test (#32)
    * create security tests
    
    * make edits to the security test
    
    * provide additional edits to the security test
    
    * will remove the mock bean for the User Repository class and remove the repository test
    
    * create security tests
    
    * make edits to the security test
    
    * provide additional edits to the security test
    
    * will remove the mock bean for the User Repository class and remove the repository test

---
+++ src/test/java/com/codingnomads/impacttracker/logic/UserServiceTest.java
@@ -0,0 +1,50 @@
+package com.codingnomads.impacttracker.logic;
+
+import com.codingnomads.impacttracker.logic.RoleRepository;
+import com.codingnomads.impacttracker.logic.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+public class UserServiceTest {
+
+    private User user;
+
+    @MockBean
+    private UserService userService;
+
+    @Before
+    public void setUp() {
+
+        user = new User();
+        user.setUsername("testuser");
+        user.setPassword("password7");
+    }
+
+    @Test
+    public void whenUserServiceGetUserByUserNameAndPassword_returnUserByUserNameAndPassword() {
+
+        Mockito.when(userService.getUserByUserNameAndPassword(user.getUsername(), user.getPassword())).thenReturn(user);
+        User userByUserNameAndPassword = userService.getUserByUserNameAndPassword("testuser", "password7");
+
+        assertThat(userByUserNameAndPassword).isEqualTo(user);
+
+        @TestConfiguration
+        class UserServiceConfiguration {
+
+            @Bean
+            public UserService userService(UserRepository userRepository, RoleRepository roleRepository, UserRoleRepository userRoleRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+                return new UserService(userRepository, roleRepository, userRoleRepository, bCryptPasswordEncoder);
+            }
+        }
+    }
+}
