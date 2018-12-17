8d81ff6ef06d0a5ccfde75b8a294f791c06f1ec1
Roi Otero Morales <roioteromorales@gmail.com>
2018-12-17 16:37:59 +0100

    Test Fixing

--- src/main/java/com/codingnomads/impacttracker/logic/user/UserService.java
+++ src/main/java/com/codingnomads/impacttracker/logic/user/UserService.java
@@ -1,32 +1,31 @@
 package com.codingnomads.impacttracker.logic.user;
 
+import com.codingnomads.impacttracker.logic.authentication.AuthenticationProvider;
 import com.codingnomads.impacttracker.model.Role;
 import com.codingnomads.impacttracker.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Resource;
-
 @Service
 public class UserService {
 
-    @Resource
     private UserRepository userRepository;
     private RoleRepository roleRepository;
     private UserRoleRepository userRoleRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private AuthenticationProvider authenticationProvider;
 
     @Autowired
-    public UserService(UserRepository userRepository, RoleRepository roleRepository, UserRoleRepository userRoleRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, UserRoleRepository userRoleRepository, BCryptPasswordEncoder bCryptPasswordEncoder, AuthenticationProvider authenticationProvider) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.userRoleRepository = userRoleRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.authenticationProvider = authenticationProvider;
     }
 
     public User getUserByUserName(String username) {
@@ -68,11 +67,13 @@ public class UserService {
 
     public Integer getCurrentUserId() {
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Authentication authentication = authenticationProvider.getAuthentication();
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
             String currentUserName = authentication.getName();
             return getUserByUserName(currentUserName).getId();
         }
         return null;
     }
+
+
 }
--- src/test/java/com/codingnomads/impacttracker/logic/UserServiceTest.java
+++ src/test/java/com/codingnomads/impacttracker/logic/UserServiceTest.java
@@ -1,33 +1,19 @@
 package com.codingnomads.impacttracker.logic;
 
-import com.codingnomads.impacttracker.data.repositories.MySqlRoleRepository;
-import com.codingnomads.impacttracker.data.repositories.MySqlUserRepository;
-import com.codingnomads.impacttracker.data.repositories.MySqlUserRoleRepository;
-import com.codingnomads.impacttracker.logic.user.RoleRepository;
-import com.codingnomads.impacttracker.model.User;
+import com.codingnomads.impacttracker.logic.authentication.AuthenticationProvider;
 import com.codingnomads.impacttracker.logic.user.UserRepository;
-import com.codingnomads.impacttracker.logic.user.UserRoleRepository;
 import com.codingnomads.impacttracker.logic.user.UserService;
+import com.codingnomads.impacttracker.model.User;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.withSettings;
 
 @RunWith(SpringRunner.class)
 public class UserServiceTest {
@@ -36,12 +22,18 @@ public class UserServiceTest {
     private static final int VALID_USER_ID = 1;
     private User user;
 
-    @MockBean
+    @Mock
+    private UserRepository userRepositoryMock;
+    @Mock
+    private AuthenticationProvider authenticationProviderMock;
+    @Mock
+    private Authentication authenticationMock;
+
+    @InjectMocks
     private UserService userService;
 
     @Before
     public void setUp() {
-
         user = new User();
         user.setUsername(VALID_USERNAME);
         user.setId(VALID_USER_ID);
@@ -49,39 +41,21 @@ public class UserServiceTest {
 
     @Test
     public void whenUserServiceGetUserByUserName_returnUserByUserName() {
+        when(userRepositoryMock.getUserByUserName(VALID_USERNAME)).thenReturn(user);
 
-        when(userService.getUserByUserName(user.getUsername())).thenReturn(user);
         User userByUserName = userService.getUserByUserName(VALID_USERNAME);
 
         assertThat(userByUserName).isEqualTo(user);
-
-        @TestConfiguration
-        class UserServiceConfiguration {
-
-            @Bean
-            public UserService userService(UserRepository userRepository, RoleRepository roleRepository, UserRoleRepository userRoleRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-                return new UserService(userRepository, roleRepository, userRoleRepository, bCryptPasswordEncoder);
-            }
-        }
     }
 
     @Test
-    public void whenGettingCurrentUserId_shouldReturnCurrentUserId () {
+    public void whenGettingCurrentUserId_shouldReturnCurrentUserId() {
+        when(authenticationProviderMock.getAuthentication()).thenReturn(authenticationMock);
+        when(authenticationMock.getName()).thenReturn(VALID_USERNAME);
+        when(userRepositoryMock.getUserByUserName(VALID_USERNAME)).thenReturn(user);
 
-        Authentication mockAuthentication = Mockito.mock(TestingAuthenticationToken.class);
-        SecurityContext mockSecurityContext = Mockito.mock(SecurityContext.class);
-        MySqlUserRepository userRepositoryMock = Mockito.mock(MySqlUserRepository.class);
-        MySqlRoleRepository roleRepositoryMock = Mockito.mock(MySqlRoleRepository.class);
-        MySqlUserRoleRepository userRoleRepositoryMock = Mockito.mock(MySqlUserRoleRepository.class);
-        BCryptPasswordEncoder bCryptPasswordEncoder = Mockito.mock(BCryptPasswordEncoder.class);
-        UserService userServiceMock = Mockito.spy(new UserService(userRepositoryMock,roleRepositoryMock,userRoleRepositoryMock,bCryptPasswordEncoder));
-        when(mockSecurityContext.getAuthentication()).thenReturn(mockAuthentication);
-        when(mockAuthentication.getName()).thenReturn(VALID_USERNAME);
-        when(userServiceMock.getUserByUserName(VALID_USERNAME)).thenReturn(user);
-        SecurityContextHolder.setContext(mockSecurityContext);
+        int currentUserId = userService.getCurrentUserId();
 
-        int output = userServiceMock.getCurrentUserId();
-
-        assertThat(output).isEqualTo(VALID_USER_ID);
+        assertThat(currentUserId).isEqualTo(VALID_USER_ID);
     }
 }
